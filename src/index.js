6c9a32038b3b398180536067660a0c56fa2782cb
Octa <octavio.macieira@aarim.com>
2022-07-27 16:47:30 -0300

    Add Styling

--- src/index.js
+++ src/index.js
@@ -3,29 +3,45 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import './style/App.css';
 
 const App = () => {
     return(
         <div className="ui container comments">
-            <ApprovalCard/>
-            <CommentDetail 
-            author={faker.name.findName()}
-            date="Today at 5:42PM"
-            image={faker.image.people()} 
-            comment={faker.lorem.lines()}
-            />
-            <CommentDetail 
-            author={faker.name.findName()} 
-            date="Yesterday at 4:32PM"
-            image={faker.image.people()} 
-            comment={faker.lorem.lines()} 
-            />
-            <CommentDetail 
-            author={faker.name.findName()} 
-            date="Yesterday at 2:32PM"
-            image={faker.image.people()} 
-            comment={faker.lorem.lines()} 
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>    
+            </ApprovalCard>
+            
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    author={faker.name.findName()}
+                    date="Today at 5:42PM"
+                    image={faker.image.people()} 
+                    comment={faker.lorem.lines()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author={faker.name.findName()} 
+                    date="Yesterday at 4:32PM"
+                    image={faker.image.people()} 
+                    comment={faker.lorem.lines()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author={faker.name.findName()} 
+                    date="Yesterday at 2:32PM"
+                    image={faker.image.people()} 
+                    comment={faker.lorem.lines()} 
+                />
+            </ApprovalCard>
         </div>
     )
 };
