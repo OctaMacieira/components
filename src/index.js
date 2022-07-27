ca940c281e8f59eb16337bfc739a65fed7025b9b
Octa <octavio.macieira@aarim.com>
2022-07-27 15:03:03 -0300

    Add Approval Cards

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return(
         <div className="ui container comments">
+            <ApprovalCard/>
             <CommentDetail 
             author={faker.name.findName()}
             date="Today at 5:42PM"
